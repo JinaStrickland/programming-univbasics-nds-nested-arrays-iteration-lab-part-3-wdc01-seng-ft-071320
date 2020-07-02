de2f20f5ed9a84a168f0f13902a8bbad7cf7d320
Jina Strickland <jina30@gmail.com>
2020-07-02 02:11:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,13 +2,13 @@
   # Combine all Strings present in the AoA into a single value and return it
 
 def join_nested_strings(src)
-  new_string = ""
+  nested_strings = ""
   row_index = 0
     while row_index < src.count do
       element_index = 0
-
+      new_string = ""
     while element_index < src[row_index].length do
-      if src[row_index][element_index] == 
+      if src[row_index][element_index] == " "
         max_value = src[row_index][element_index]
       end
         element_index += 1
