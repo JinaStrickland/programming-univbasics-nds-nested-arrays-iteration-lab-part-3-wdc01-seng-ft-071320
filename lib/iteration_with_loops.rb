0f6c441b3f0db9f7577eb23611c59b1b2a9581b0
Jina Strickland <jina30@gmail.com>
2020-07-02 02:27:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,34 +8,15 @@ def join_nested_strings(src)
     while row_index < src.count do
       inner_index = 0
       new_string = ""
-    while inner_index < src[row_index].length do
+    while inner_index < src[row_index].count do
       if src[row_index][inner_index].class == String
-        nested_strings << src[row_index][inner_index] + " "
+        new_string = src[row_index][inner_index]
       end
         inner_index += 1
     end
+      nested_strings << src[row_index][inner_index] + " "
       row_index += 1
     end
       low_temp_src
 
-end
-
-
-
-def find_min_in_nested_arrays(src)
-low_temp_src = []
-row_index = 0
-  while row_index < src.count do
-    element_index = 0
-    max_value = 1000
-  while element_index < src[row_index].length do
-    if src[row_index][element_index] < max_value
-      max_value = src[row_index][element_index]
-    end
-      element_index += 1
-  end
-    low_temp_src << max_value
-    row_index += 1
-  end
-    low_temp_src
 end
