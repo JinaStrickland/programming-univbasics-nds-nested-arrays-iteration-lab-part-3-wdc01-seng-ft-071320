f100edfca2c44ec333f721ed5237c16a4bd07a1a
Jina Strickland <jina30@gmail.com>
2020-07-02 02:39:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ def join_nested_strings(src)
       inner_index = 0
     while inner_index < src[row_index].count do
       if src[row_index][inner_index].class == String
-        nested_strings << src[row_index][inner_index] + " "
+        nested_strings << src[row_index][inner_index] + ""
       end
         inner_index += 1
     end
@@ -17,23 +17,3 @@ def join_nested_strings(src)
   end
     nested_strings.join("")
 end
-
-
-
-# def join_nested_strings(src)
-#
-#   row_index = 0
-#   nested_strings = []
-#     while row_index < src.count do
-#       inner_index = 0
-#       new_string = ""
-#     while inner_index < src[row_index].count do
-#       if src[row_index][inner_index].class == String
-#         new_string = src[row_index][inner_index]
-#       end
-#         inner_index += 1
-#     end
-#       nested_strings.push << src[row_index][inner_index] + ""
-#       row_index += 1
-#     end
-# end
