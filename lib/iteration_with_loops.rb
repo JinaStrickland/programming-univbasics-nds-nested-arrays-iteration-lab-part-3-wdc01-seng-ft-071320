1b8e4f20f71a3cee44cbb0b9c7f2a9ac54901fdb
Jina Strickland <jina30@gmail.com>
2020-07-02 02:43:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@
 
 def join_nested_strings(src)
   row_index = 0
-  nested_strings = ""
+  nested_strings = []
     while row_index < src.count do
       inner_index = 0
     while inner_index < src[row_index].count do
