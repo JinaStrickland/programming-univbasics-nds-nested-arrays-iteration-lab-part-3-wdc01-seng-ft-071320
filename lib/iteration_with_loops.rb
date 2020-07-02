826577ae6c98bf34dbf3c3461a5cccd6fa009ea5
Jina Strickland <jina30@gmail.com>
2020-07-02 02:08:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,9 +6,9 @@ def join_nested_strings(src)
   row_index = 0
     while row_index < src.count do
       element_index = 0
-      
+
     while element_index < src[row_index].length do
-      if src[row_index][element_index] < max_value
+      if src[row_index][element_index] == 
         max_value = src[row_index][element_index]
       end
         element_index += 1
