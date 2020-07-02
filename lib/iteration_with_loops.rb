071b04d002c861da8dc32db1cc0cc3e3fb665992
Jina Strickland <jina30@gmail.com>
2020-07-02 02:07:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,41 @@
-def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
-  
+
+def join_nested_strings(src)
+  new_string = ""
+  row_index = 0
+    while row_index < src.count do
+      element_index = 0
+      
+    while element_index < src[row_index].length do
+      if src[row_index][element_index] < max_value
+        max_value = src[row_index][element_index]
+      end
+        element_index += 1
+    end
+      low_temp_src << max_value
+      row_index += 1
+    end
+      low_temp_src
+
+end
 
 
+
+def find_min_in_nested_arrays(src)
+low_temp_src = []
+row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    max_value = 1000
+  while element_index < src[row_index].length do
+    if src[row_index][element_index] < max_value
+      max_value = src[row_index][element_index]
+    end
+      element_index += 1
+  end
+    low_temp_src << max_value
+    row_index += 1
+  end
+    low_temp_src
 end
